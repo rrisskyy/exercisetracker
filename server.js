a2c78e2f4ee701c13ae6e75cf48dde01bdd3291f
Risky Kurniawan <73045161+rrisskyy@users.noreply.github.com>
2021-12-01 21:38:42 +0800

    Done

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ const User = new mongoose.model('User', userSchema);
 // 3. You can make a GET request to /api/users to get a list of all users.
 // 4. The GET request to /api/users returns an array.
 // 5. Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
+
 app.post("/api/users", (req, res) => {
   const user = new User({ username: req.body.username });
   user.save((err) => {
@@ -74,9 +75,9 @@ app.get("/api/users", (req, res) => {
 });
 
 
-
 // 6. You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
 // 7. The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
+
 app.post("/api/users/:_id/exercises", (req, res) => {
   const userId = req.params._id;
 
@@ -104,27 +105,26 @@ app.post("/api/users/:_id/exercises", (req, res) => {
 });
 
 
-
 // 8. A GET request to /api/users/:id/logs will return the user object with a log array of all the exercises added.
 // 9. Each item in the log array that is returned from GET /api/users/:id/logs is an object that should have a description, duration, and date properties.
 // 10. The description property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.
 // 11. The duration property of any object in the log array that is returned from GET /api/users/:id/logs should be a number.
 // 12. The date property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.. Use the dateString format of the Date API.
 
-
 app.get("/api/users/:_id/logs", (req, res) => {
   let userId = req.params._id;
   let dFrom = req.query.from || '0000-00-00';
   let dTo = req.query.to || '9999-99-99';
-  let limit = +req.query.limit || 10000;
+  let limit = +req.query.limit || 999;
+
   User.findById(userId, function (err, user) {
     if (err) return console.log(err);
-    let newLog = user.exercices.filter(e => e.date >= dFrom && e.date <= dTo)
-      .map(e => (
+    let newLog = user.exercices.filter(res => res.date >= dFrom && res.date <= dTo)
+      .map(result => (
         {
-          description: e.description, 
-          duration: e.duration, 
-          date: e.date
+          description: result.description, 
+          duration: result.duration, 
+          date: result.date
         }
       ))
       .slice(0,limit);
@@ -139,25 +139,11 @@ app.get("/api/users/:_id/logs", (req, res) => {
   });
 });
 
-
-
-
-
-
-// from=2021-11-17&to=2021-12-2&limit=3
-
-
-
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
